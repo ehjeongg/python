47dd7bcc7d20619f04c3a283dec09d7508910803
grace <ehjeong@in-soft.co.kr>
2022-12-09 17:53:00 +0900

    120 회사-오후2

--- pythonBasicStudy/python_basic_practice/test17.py
+++ pythonBasicStudy/python_basic_practice/test17.py
@@ -21,20 +21,63 @@
 #         else:
 #             break
 
-flag = True
-n=0 #반복되지 않는 변수등은 while문 같은 반복문 밖에서 선언하기
-print('총 5번 평/윤년을 확인할 수 있습니다.')
-while flag:
-    # n = 0 -> n=0이라는 변수 선언이 반복문 안에 들어오면 다른 조건과 같이 반복되기때문에 무한루프에 돌게됨
-    year = int(input('년도 입력 : '))
-    n+=1
-    if (year %4 == 0 and year % 100 !=0) or year % 400==0 : #여러가지 조건식을 한줄에 사용하는 방법
-        n += 1
-        print( '{}년은 윤년이다'.format(year))
 
+
+# flag = True
+# n=0 #반복되지 않는 변수등은 while문 같은 반복문 밖에서 선언하기
+# print('총 5번 평/윤년을 확인할 수 있습니다.')
+# while flag:
+#     # n = 0 -> n=0이라는 변수 선언이 반복문 안에 들어오면 다른 조건과 같이 반복되기때문에 무한루프에 돌게됨
+#     year = int(input('년도 입력 : '))
+#     n+=1
+#     if (year %4 == 0 and year % 100 !=0) or year % 400==0 : #여러가지 조건식을 한줄에 사용하는 방법
+#         n += 1
+#         print( '{}년은 윤년이다'.format(year))
+#
+#     else:
+#         n += 1
+#         print( '{}년은 평년이다'.format(year))
+#     if n > 5:
+#         flag = False
+#         print('\n확인 가능한 5회권 모두 소진되었습니다. ')
+
+
+# #정상 코드
+# flag = True
+# n=0
+# year=0
+# print('총 5번 평/윤년을 확인할 수 있습니다.')
+# while flag:
+#     if n < 5:
+#         year = int(input('년도 입력 : '))
+#         n += 1
+#
+#         if (year %4 == 0 and year % 100 !=0) or year % 400==0 : #여러가지 조건식을 한줄에 사용하는 방법
+#             n += 1
+#             print( '{}년은 윤년이다'.format(year))
+#
+#         else:
+#             n += 1
+#             print( '{}년은 평년이다'.format(year))
+#     else:
+#         flag = False
+#         print('\n확인 가능한 5회권 모두 소진되었습니다. ')
+
+
+
+#정상코드
+n=0
+year=0
+print('총 5번 평/윤년을 확인할 수 있습니다.')
+while True:
+    if n < 5: #가장 처음 횟수가 n=0부터 시작하기 때문에 5미만의 숫자가 주어지는 것 / n=0부터 1회이기 때문 -> n = 0,1,2,3,4(총 5회)
+        year = int(input('년도 입력 : '))
+        if (year %4 == 0 and year % 100 !=0) or year % 400==0 : #여러가지 조건식을 한줄에 사용하는 방법
+            n += 1
+            print( '{}년은 윤년이다'.format(year))
+        else:
+            n += 1
+            print( '{}년은 평년이다'.format(year))
     else:
-        n += 1
-        print( '{}년은 평년이다'.format(year))
-    if n > 6:
-        flag = False
         print('\n확인 가능한 5회권 모두 소진되었습니다. ')
+        break
